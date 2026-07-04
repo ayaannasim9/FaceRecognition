b85faeac031c2680cc9b79c67fbf6bd92975198c
ayaannasim9 <ayaannasim9@gmail.com>
2026-07-04 16:30:14 +0100

    Added identify_face function
    Basic function to fetch the person's name and display the majority class in the top matches. Script now gives a single person name

--- face_recog.py
+++ face_recog.py
@@ -1,10 +1,22 @@
 from deepface import DeepFace
+from pathlib import Path
 
-query_image="/Users/ayaannasim/Documents/face_recognition/face_db/Jake_Gyllenhaal/3227.jpg"
+query_image="/Users/ayaannasim/Documents/face_recognition/face_db/Adam_Sandler/0379.jpg"
 database="face_db"
 results=DeepFace.find(img_path=query_image,
                       db_path=database,
                       model_name="VGG-Face",
                       detector_backend="skip")
 matches=results[0]
-print(matches[["identity", "distance"]].head())
+# print(matches[["identity", "distance"]].head())
+
+def identify_face():
+    top_matches=matches[["identity"]].head().copy()
+    top_matches["person"]=top_matches["identity"].apply(lambda path: Path(path).parent.name)
+
+    top_match=top_matches["person"].mode()
+    predicted_person=top_match.iloc[0]
+    print(f"predicted person : {predicted_person}")
+    
+    
+identify_face()
